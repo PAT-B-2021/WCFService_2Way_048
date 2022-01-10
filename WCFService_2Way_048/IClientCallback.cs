5dade3ebad4805d80ba7cdf087a19589df7cc9bc
Kusuka-Masako <79459969+Kusuka-Masako@users.noreply.github.com>
2022-01-10 07:14:18 +0700

    Add project files.

---
+++ WCFService_2Way_048/IClientCallback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService_2Way_048
+{
+
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string use, string pesan);
+    }
+}
